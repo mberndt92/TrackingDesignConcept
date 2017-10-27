e3b6b9064c199ff65b817243571bc2d73389f513
mberndt92 <maximilian.berndt@foodora.com>
2017-10-27 12:15:15 +0200

    Initial Commit

---
+++ TrackingDesignProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrackingDesignProject
+//
+//  Created by Berndt, Maximilian on 27.10.17.
+//  Copyright © 2017 mb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
